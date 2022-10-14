9c5decbcda4fd00f6bdf0bbce97a0d77e95c0b9f
Andy Labrada <alabrada0810@gmail.com>
2022-10-13 23:56:43 -0400

    First upload

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,57 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Andy on 10/13/22.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+           print("Camera is available 📸")
+           vc.sourceType = .camera
+        } else {
+           print("Camera 🚫 available so we will use photo library instead")
+           vc.sourceType = .photoLibrary
+        }
+
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
