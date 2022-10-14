a22be6a7976f9830a5dea4653c5873a1b90f1546
Andy Labrada <alabrada0810@gmail.com>
2022-10-14 01:10:45 -0400

    Finished part 1

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -2,15 +2,16 @@
 //  CameraViewController.swift
 //  Parstagram
 //
-//  Created by Andy on 10/13/22.
+//  Created by Andy on 10/14/22.
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var commentField: UITextField!
     
     override func viewDidLoad() {
@@ -21,27 +22,57 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
 
     @IBAction func onSubmit(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            }
+            else {
+                print("error")
+            }
+            
+        }
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
+        
         let vc = UIImagePickerController()
         vc.delegate = self
         vc.allowsEditing = true
 
+        // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
            print("Camera is available 📸")
            vc.sourceType = .camera
         } else {
-           print("Camera 🚫 available so we will use photo library instead")
+           print("Camera not available so we will use photo library instead")
            vc.sourceType = .photoLibrary
         }
 
         self.present(vc, animated: true, completion: nil)
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
     }
     
     /*
